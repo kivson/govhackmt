5207129b48c891af5b5f3471357e3e4473e07307
Gabriel Simplício Dias <Gabriel Simplcio Dias>
2016-11-06 10:57:28 -0300

    models

--- src/app/estatisticas/estatisticas.component.ts
+++ src/app/estatisticas/estatisticas.component.ts
@@ -9,13 +9,13 @@ import { OuvidoriaService } from '../ouvidoria.service';
 export class EstatisticasComponent implements OnInit {
 
   dados: Array<any>;
-  duvidasDados: Array<Number>;
+  duvidasDados: Array<number> = [7,4,200];
   reclamacoesDados: Array<Number>;
   elogiosDados: Array<Number>;
   telefonemasQuantidade: Number = 0; // Array.reduce #dica
   enderecoQuantidade: Number = 0;
-  tipoDoGrafico: String = 'line'; // Não alterar
-  labelsDoGrafico: Array<String> = ['Mês', 'Semana', 'Dia']; // Não alterar
+  tipoDoGrafico: string = 'doughnut'; // Não alterar
+  labelsDoGrafico: Array<String> = ['Elogios', 'Reclamações', 'Dúvidas']; // Não alterar
 
   constructor(private _ouvidoriaService: OuvidoriaService) { }
 
@@ -23,6 +23,25 @@ export class EstatisticasComponent implements OnInit {
     this._ouvidoriaService
       .obterDados()
       .subscribe(response => {
+        var intents = response.map(x => x.intents[0].intent);
+        console.log(intents);
+
+        // var inputs = response.map(x => x.input.text);
+        // console.log(inputs);
+
+        var entities = response.map(x => x.entities && x.entities.length > 0 ? x.entities[0].value : null).filter(x => x != null);
+        console.log(entities);
+
+        var elogios = response.filter(x => x.entities && x.entities.length > 0 && x.entities[0].value == "elogio");
+        console.log(elogios.length);
+
+        var reclamacoes = response.filter(x => x.entities && x.entities.length > 0 && x.entities[0].value == "reclamação");
+        console.log(reclamacoes.length);
+
+        this.duvidasDados.push(elogios.length);
+        this.duvidasDados.push(reclamacoes.length);
+        this.duvidasDados.push(response.length - (elogios.length + reclamacoes.length));
+
         // TODO: map response => numeros de cada estatisticas;
         this.dados = response;
       }, err => {
---
+++ src/app/models/face_msg.model.ts
@@ -0,0 +1,12 @@
+export class FaceMsg {
+
+    entry: Array<MsgEntry> = new Array<MsgEntry>();
+
+}
+
+export class MsgEntry {
+
+    id: string;
+    time: number;
+
+}
